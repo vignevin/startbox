659a186d8894521c7e6b6acdf88d1eb1fd16b7ec
HerveData <herve.maire22@gmail.com>
2025-06-06 16:14:51 +0200

    add ‘fake function’ to be able to do ?startbox

---
+++ R/startbox-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' startbox
+#'
+#' This package is used to manage and display experimental plant protection data.
+#' 
+#' To get started, see the vignette: `vignette("startbox")`.
+#'
+#' @docType package
+#' @name startbox
+#' @aliases startbox-package
+#' @seealso \link{vignette("startbox")}
+NULL
